401c875b7b9796ccf2c07f66563727fb22179717
Yulia <fretkafretka.j@gmail.com>
2024-10-24 21:11:15 +0700

    Создание Parameter Object для сценария с добавлением клиента

--- add_client.py
+++ add_client.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from client import Client
+
 
 class AddClient(unittest.TestCase):
     def setUp(self):
@@ -15,10 +17,10 @@ class AddClient(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.open_client_page(wd)
-        self.create_client(wd, firstname="firstqweqwe", middlename="middlejhfghkdjfhfgdf", lastname="lastjvhfdkjghg", nickname="nicjfhdjkghf",
+        self.create_client(wd, Client(firstname="firstqweqwe", middlename="middlejhfghkdjfhfgdf", lastname="lastjvhfdkjghg", nickname="nicjfhdjkghf",
                            title="titltfjdjfhjdhgh", company="companyhdjfkshkfs", address="adreddkdfkjgldfjgfdjkfjgldfkjg", homephone="85654752453426",
                            mobilephone="987988989", workphone="1074567456757555", fax="2475858585", emaleone="hdhfwghfg@hgjhfg.com",
-                           emailtwo="fhgerhgjrhhh@ffjgjg.com", emailthree="fhfh@.com", homepage="qwe", daybirthday="2", monthbirthday="February", yearbirthday="2006", annday="5", annmonth="July", annyear="2008")
+                           emailtwo="fhgerhgjrhhh@ffjgjg.com", emailthree="fhfh@.com", homepage="qwe", daybirthday="2", monthbirthday="February", yearbirthday="2006", annday="5", annmonth="July", annyear="2008"))
         self.return_home_page(wd)
         self.logout(wd)
 
@@ -27,15 +29,15 @@ class AddClient(unittest.TestCase):
             self.open_home_page(wd)
             self.login(wd, username="admin", password="secret")
             self.open_client_page(wd)
-            self.create_client(wd, firstname="", middlename="",
+            self.create_client(wd, Client(firstname="", middlename="",
                                lastname="", nickname="",
                                title="", company="",
                                address="", homephone="",
                                mobilephone="", workphone="", fax="",
                                emaleone="",
                                emailtwo="", emailthree="", homepage="",
-                               daybirthday="4", monthbirthday="March", yearbirthday="2003", annday="4",
-                               annmonth="April", annyear="2000")
+                               daybirthday="-", monthbirthday="-", yearbirthday="-", annday="-",
+                               annmonth="-", annyear="-"))
             self.return_home_page(wd)
             self.logout(wd)
 
@@ -45,73 +47,71 @@ class AddClient(unittest.TestCase):
     def return_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def create_client(self, wd, firstname, middlename, lastname, nickname, title, company, address, homephone,
-                      mobilephone, workphone, fax, emaleone, emailtwo, emailthree, homepage, daybirthday, monthbirthday,
-                      yearbirthday, annday, annmonth, annyear):
+    def create_client(self, wd, client):
         # fill client form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(firstname)
+        wd.find_element_by_name("firstname").send_keys(client.firstname)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(middlename)
+        wd.find_element_by_name("middlename").send_keys(client.middlename)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(lastname)
+        wd.find_element_by_name("lastname").send_keys(client.lastname)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys(nickname)
+        wd.find_element_by_name("nickname").send_keys(client.nickname)
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").clear()
-        wd.find_element_by_name("title").send_keys(title)
+        wd.find_element_by_name("title").send_keys(client.title)
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys(company)
+        wd.find_element_by_name("company").send_keys(client.company)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(address)
+        wd.find_element_by_name("address").send_keys(client.address)
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys(homephone)
+        wd.find_element_by_name("home").send_keys(client.homephone)
         wd.find_element_by_name("mobile").click()
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys(mobilephone)
+        wd.find_element_by_name("mobile").send_keys(client.mobilephone)
         wd.find_element_by_name("work").click()
         wd.find_element_by_name("work").clear()
-        wd.find_element_by_name("work").send_keys(workphone)
+        wd.find_element_by_name("work").send_keys(client.workphone)
         wd.find_element_by_name("fax").click()
         wd.find_element_by_name("fax").clear()
-        wd.find_element_by_name("fax").send_keys(fax)
+        wd.find_element_by_name("fax").send_keys(client.fax)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys(emaleone)
+        wd.find_element_by_name("email").send_keys(client.emaleone)
         wd.find_element_by_name("email2").click()
         wd.find_element_by_name("email2").clear()
-        wd.find_element_by_name("email2").send_keys(emailtwo)
+        wd.find_element_by_name("email2").send_keys(client.emailtwo)
         wd.find_element_by_name("email3").click()
         wd.find_element_by_name("email3").clear()
-        wd.find_element_by_name("email3").send_keys(emailthree)
+        wd.find_element_by_name("email3").send_keys(client.emailthree)
         wd.find_element_by_name("homepage").click()
         wd.find_element_by_name("homepage").clear()
-        wd.find_element_by_name("homepage").send_keys(homepage)
+        wd.find_element_by_name("homepage").send_keys(client.homepage)
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text(daybirthday)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(client.daybirthday)
         wd.find_element_by_xpath("//option[@value='2']").click()
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(monthbirthday)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(client.monthbirthday)
         wd.find_element_by_xpath("//option[@value='February']").click()
         wd.find_element_by_name("byear").click()
         wd.find_element_by_name("byear").clear()
-        wd.find_element_by_name("byear").send_keys(yearbirthday)
+        wd.find_element_by_name("byear").send_keys(client.yearbirthday)
         wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text(annday)
+        Select(wd.find_element_by_name("aday")).select_by_visible_text(client.annday)
         wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[7]").click()
         wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text(annmonth)
+        Select(wd.find_element_by_name("amonth")).select_by_visible_text(client.annmonth)
         wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[8]").click()
         wd.find_element_by_name("ayear").click()
         wd.find_element_by_name("ayear").clear()
-        wd.find_element_by_name("ayear").send_keys(annyear)
+        wd.find_element_by_name("ayear").send_keys(client.annyear)
         # create client
         wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
 
---
+++ client.py
@@ -0,0 +1,24 @@
+class Client:
+
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homephone, mobilephone, workphone, fax, emaleone, emailtwo, emailthree, homepage, daybirthday, monthbirthday, yearbirthday, annday, annmonth, annyear):
+        self.firstname = firstname
+        self.middlename =middlename
+        self.lastname = lastname
+        self.nickname = nickname
+        self.title = title
+        self.company = company
+        self.address = address
+        self.homephone =homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.fax = fax
+        self.emaleone = emaleone
+        self.emailtwo = emailtwo
+        self.emailthree = emailthree
+        self.homepage = homepage
+        self.daybirthday =daybirthday
+        self.monthbirthday = monthbirthday
+        self.yearbirthday = yearbirthday
+        self.annday = annday
+        self.annmonth = annmonth
+        self.annyear = annyear
