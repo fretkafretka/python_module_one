9aa3ae7484736ec93698e5f7d9735194b56f985f
Yulia <fretkafretka.j@gmail.com>
2024-10-31 22:22:46 +0700

    Задание №5: Реализовать двухуровневую архитектуру тестов

--- test/client_test.py
+++ test/client_test.py
@@ -10,24 +10,24 @@ def app(request):
     return fixture
 
 def test_add_client(app):
-    app.login(username="admin", password="secret")
-    app.create_client(Client(firstname="firstqweqwe", middlename="middlejhfghkdjfhfgdf", lastname="lastjvhfdkjghg", nickname="nicjfhdjkghf",
-                           title="titltfjdjfhjdhgh", company="companyhdjfkshkfs", address="adreddkdfkjgldfjgfdjkfjgldfkjg", homephone="85654752453426",
-                           mobilephone="987988989", workphone="1074567456757555", fax="2475858585", emaleone="hdhfwghfg@hgjhfg.com",
-                           emailtwo="fhgerhgjrhhh@ffjgjg.com", emailthree="fhfh@.com", homepage="qwe", daybirthday="2", monthbirthday="February", yearbirthday="2006", annday="5", annmonth="July", annyear="2008"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.client.create(Client(firstname="firstqweqwe", middlename="middlejhfghkdjfhfgdf", lastname="lastjvhfdkjghg", nickname="nicjfhdjkghf",
+                             title="titltfjdjfhjdhgh", company="companyhdjfkshkfs", address="adreddkdfkjgldfjgfdjkfjgldfkjg", homephone="85654752453426",
+                             mobilephone="987988989", workphone="1074567456757555", fax="2475858585", emaleone="hdhfwghfg@hgjhfg.com",
+                             emailtwo="fhgerhgjrhhh@ffjgjg.com", emailthree="fhfh@.com", homepage="qwe", daybirthday="2", monthbirthday="February", yearbirthday="2006", annday="5", annmonth="July", annyear="2008"))
+    app.session.logout()
 
 def test_add_empty_client(app):
-    app.login(username="admin", password="secret")
-    app.create_client(Client(firstname="", middlename="",
-                               lastname="", nickname="",
-                               title="", company="",
-                               address="", homephone="",
-                               mobilephone="", workphone="", fax="",
-                               emaleone="",
-                               emailtwo="", emailthree="", homepage="",
-                               daybirthday="-", monthbirthday="-", yearbirthday="-", annday="-",
-                               annmonth="-", annyear="-"))
-    app.logout()
+    app.session.login(username="admin", password="secret")
+    app.client.create(Client(firstname="", middlename="",
+                             lastname="", nickname="",
+                             title="", company="",
+                             address="", homephone="",
+                             mobilephone="", workphone="", fax="",
+                             emaleone="",
+                             emailtwo="", emailthree="", homepage="",
+                             daybirthday="-", monthbirthday="-", yearbirthday="-", annday="-",
+                             annmonth="-", annyear="-"))
+    app.session.logout()
 
 
